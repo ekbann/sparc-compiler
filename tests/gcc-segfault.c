1ddbc463b640f12750ed5c911e906780be11beec
ekbann <ellery@bann.com>
2022-11-23 13:17:07 -0300

    Added gcc-segfault.c

---
+++ tests/gcc-segfault.c
@@ -0,0 +1,12 @@
+int main() {
+	/* debug(symtab_dump) */
+	'b';
+	/* debug(symtab_dump) */
+	"z";
+	/* debug(symtab_dump) */
+	'c';
+	/* debug(symtab_dump) */
+	"h";
+	/* debug(symtab_dump) */
+}
+/* debug(statement_dump) */
